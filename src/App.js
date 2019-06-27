93d64325a72bb7978ff67abd999ff968b25e52be
Morgan-Mischo <goramorgan@gmail.com>
2019-06-27 11:20:57 -0600

    working on app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Add from './components/Add'; 
+import Afternoon from './components/Afternoon';
+import Evening from './components/Evening'; 
+import Morning from './components/Morning';
+
+class App extends Component {
+  constructor() {
+    super(); 
+    this.state ={
+      newDay: '', 
+      tasks: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+    .get('/api/tasks')
+    .then(res => {
+      this.setState({ tasks: res.data }); 
+    })
+    .catch(err => {
+      console.log('err from server', err); 
+    }); 
+  }
+
+  editTask = (id) => {
+    
+  }
+
+  render() {
+    return(
+      <div className="App">
+      <div className="header">
+        Header
+        <div className="tasks">Tasks</div>
+        <div className="finished">Finished</div>
+        <div className="add">Add</div>
+        <div className="new-day">New Day</div>
+      </div>
+      <div className="morning">Morning</div>
+      <div className="afternoon">Afternoon</div>
+      <div className="evening">Evening</div>
+      </div>
+      
+      
+    )
+  }
 }
 
 export default App;
